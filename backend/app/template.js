8015fcd220bf60e15fba81dc77ef0ed1a53c9dc4
Capevace <capevace@gmail.com>
2017-06-12 17:50:08 +0200

    Fixes template

--- backend/app/template.js
+++ backend/app/template.js
@@ -16,21 +16,5 @@ const htmlTemplate = fs.readFileSync(
 module.exports = function render(userAgent, serverSideScreenClass) {
   const renderedApp = renderBundle(userAgent, serverSideScreenClass);
 
-  return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-        <meta name="description" content="An easy way to share your clipboard across your devices.">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <title>Clippo - Share your Clipboard</title>
-      </head>
-      <body>
-        <div id="app">${renderedApp}</div>
-        <script type="text/javascript" src="bundle.js"></script>
-      </body>
-    </html>
-  `;
+  return htmlTemplate.replace('{{RENDERED-APP-CONTENT}}', renderedApp);
 };
